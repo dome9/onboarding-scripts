8d0b12518dbfc89a74a24248c13a7b0b847a60ac
Matt A <mambroziak@users.noreply.github.com>
2019-05-24 17:19:57 -0400

    end to end tests functioning

--- snapshot_assessment/README.md
+++ snapshot_assessment/README.md
@@ -1,13 +1,12 @@
-# **Snapshot Compliance Assessments (from one organization)** #
+# **Snapshot Compliance Assessments** #
 A command line tool to run a one-time snapshot compliance assessment of a cloud account.<br/>
 AWS is only supported in this beta and defaults to the NIST 800-53 ruleset.
 
 ## Requirements ##
 * Python 3.6 or later
-( Can be Download <a href="https://nodejs.org">here</a> )
 * AWS account with Dome9-Connect role already deployed. You will need:
-* -The Dome9 Role ARN (e.g. arn:aws:iam::012345678912:role/Dome9-Connect)
-* -The External ID used for the role. (e.g. bkbj00xuTAM102IceF0s8bX6)
+** -The Dome9 Role ARN (e.g. arn:aws:iam::012345678912:role/Dome9-Connect)
+** -The External ID used for the role. (e.g. bkbj00xuTAM102IceF0s8bX6)
 
 
 ## Installation ##
@@ -27,11 +26,12 @@ AWS is only supported in this beta and defaults to the NIST 800-53 ruleset.
 2. Run the command 
 ```
 Syntax: python snapshot_assessment.py <aws|azure|gcp> [options] 
-python snapshot_assessment.py aws --name testaccount --arn arn:aws:iam::012345678912:role/Dome9-Connect --externalid bkbj00xuTAM102IceF0s8bX6 --email user@domain.com
+python snapshot_assessment.py aws --name testaccount --arn arn:aws:iam::012345678912:role/Dome9-Connect --externalid bkbj00xuTAM102IceF0s8bX6 --email user@domain.com --delay 30
 ```
 
 ### Command Line arguments ###
-* --name : Friendly name of the account you are onboarding (No Spaces) (**required**).
+* --name : Friendly name of the account you are onboarding (No spaces) (**required**).
 * --arn : The role ARN of the AWS Dome9-Connect role (**required**). 
 * --externalid : The external ID used when created the AWS Dome9-Connect role (**required**).
 * --email : E-mail address to send compliance report (**required**).
+* --delay : Delay (in minutes) to wait for initial cloud account sync to complete. Default is 30.
--- snapshot_assessment/snapshot_assessment.py
+++ snapshot_assessment/snapshot_assessment.py
@@ -11,10 +11,12 @@ import random, string
 from requests.auth import HTTPBasicAuth
 from requests.exceptions import HTTPError
 from datetime import datetime
+from datetime import timedelta 
 
 d9id = ''
 d9secret = ''
 cloudid = ''
+print('\n:: Dome9 Snapshot Assessment :: \nExecution time: ' + str(datetime.now()) + '\n')
 
 def add_aws_account(name, arn, extid):
 
@@ -22,9 +24,19 @@ def add_aws_account(name, arn, extid):
     urldata = {"name": name, "credentials": {"arn": arn, "secret": extid, "type": "RoleBased", "isReadOnly": "true"}, "fullProtection": "false"}
     headers = {'content-type': 'application/json'}
 
-    print('Calling the Dome9 API with all required info to add AWS account.')
+    print('\nAdding target AWS account to Dome9...')
 
-    resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+    try:
+        resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+        
+        # If the response was successful, no Exception will be raised
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success!')
     
     if resp.status_code == 201:
         resp = json.loads(resp.content)
@@ -39,14 +51,27 @@ def add_aws_account(name, arn, extid):
         print(resp)
         return False
         
-def add_notification_policy(name, email):
+def add_notification_policy(name, email, cronexpression):
 
     url = "https://api.dome9.com/v2/Compliance/ContinuousComplianceNotification"
-    urldata = {"name":name,"description":"","alertsConsole":False,"scheduledReport":{"emailSendingState":"Disabled"},"changeDetection":{"emailSendingState":"Enabled","emailPerFindingSendingState":"Disabled","snsSendingState":"Disabled","externalTicketCreatingState":"Disabled","awsSecurityHubIntegrationState":"Disabled","emailData":{"recipients":[email]}},"gcpSecurityCommandCenterIntegration":{"state":"Disabled"}}
-    headers = {'content-type': 'application/json'}
-    print('Creating Notification Policy')
     
-    resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+    #urldata = {"name":name,"description":"","alertsConsole":False,"scheduledReport":{"emailSendingState":"Disabled"},"changeDetection":{"emailSendingState":"Enabled","emailPerFindingSendingState":"Disabled","snsSendingState":"Disabled","externalTicketCreatingState":"Disabled","awsSecurityHubIntegrationState":"Disabled","emailData":{"recipients":[email]}},"gcpSecurityCommandCenterIntegration":{"state":"Disabled"}}
+    urldata = {"name":name,"description":"","alertsConsole":False,"scheduledReport":{"emailSendingState":"Enabled","scheduleData":{"cronExpression":cronexpression,"type":"Detailed","recipients":[email]}},"changeDetection":{"emailSendingState":"Disabled","emailPerFindingSendingState":"Disabled","snsSendingState":"Disabled","externalTicketCreatingState":"Disabled","awsSecurityHubIntegrationState":"Disabled"},"gcpSecurityCommandCenterIntegration":{"state":"Disabled"}}
+
+    headers = {'content-type': 'application/json'}
+    print('\nCreating Notification Policy...')
+    
+    try:
+        resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+        
+        # If the response was successful, no Exception will be raised
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success!')
 
     if resp.status_code == 201:
         resp = json.loads(resp.content)
@@ -67,7 +92,7 @@ def add_cc_policy(d9cloudaccountid, extaccountid, notificationid, rulesetid):
     url = "https://api.dome9.com/v2/Compliance/ContinuousCompliancePolicy/ContinuousCompliancePolicies"
     urldata = [{"cloudAccountId":d9cloudaccountid,"bundleId":rulesetid,"externalAccountId":extaccountid,"cloudAccountType":cloudid,"notificationIds":[notificationid],"product":"compliance"}]
     headers = {'content-type': 'application/json'}
-    print('Creating Continuous Compliance Policy')
+    print('\nCreating Continuous Compliance Policy...')
 
     try:
         resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
@@ -95,12 +120,11 @@ def add_cc_policy(d9cloudaccountid, extaccountid, notificationid, rulesetid):
         print(resp)
         return False
 
-def send_all_alerts(notificationpolicyid, compliancepolicyid):
-
-    url = 'https://api.dome9.com/v2/Compliance/ContinuousComplianceNotification/PublishOpenedFindings/' + notificationpolicyid
-    urldata = {"notificationTargets":["IndexElasticSearch"],"specificPolicies":[compliancepolicyid]}
+def run_assessment(d9cloudaccountid, rulesetid):
+    url = "https://api.dome9.com/v2/assessment/bundleV2"
+    urldata = {"Id": rulesetid, "CloudAccountId": d9cloudaccountid, "CloudAccountType": cloudid, "Region":""}
     headers = {'content-type': 'application/json'}
-    print('Creating Continuous Compliance Policy')
+    print('\nRunning Compliance Assessment: ' + str(rulesetid))
 
     try:
         resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
@@ -114,28 +138,6 @@ def send_all_alerts(notificationpolicyid, compliancepolicyid):
     else:
         print('Success!')
 
-    if resp.status_code == 201:
-        resp = json.loads(resp.content)
-        print('Dome9 Send all alerts successful. workflowId: ' + resp['workflowId'])
-        return resp['workflowId']
-    
-    elif resp.status_code == 400:
-        print('Dome9 Send all alerts bad request.')
-        print(resp)
-
-    else:
-        print('Error when attempting to send all alerts.')
-        print(resp)
-        return False
-
-def run_assessment(d9cloudaccountid, rulesetid):
-    url = "https://api.dome9.com/v2/assessment/bundleV2"
-    urldata = {"Id": rulesetid, "CloudAccountId": d9cloudaccountid, "CloudAccountType": cloudid, "Region":""}
-    headers = {'content-type': 'application/json'}
-    print('Calling the Dome9 API to run compliance assessment.')
-
-    resp = requests.post(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
-
     if resp.status_code == 200:
         resp = json.loads(resp.content)
         print('Compliance Assessment completed successfully. \nView report at: https://secure.dome9.com/v2/compliance-engine/result/' + str(resp['id']))
@@ -146,9 +148,70 @@ def run_assessment(d9cloudaccountid, rulesetid):
         print(resp.content)
         return False
     
-def remove_aws_account(accountid):
-    print('test')
+def remove_cloud_account(id):
 
+    url = "https://api.dome9.com/v2/cloudaccounts/" + id
+    urldata = {}
+    headers = {'content-type': 'application/json'}
+    print('\nRemoving Cloud Account...')
+
+    try:
+        resp = requests.delete(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+        
+        # If the response was successful, no Exception will be raised
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success!')
+    
+def unassociate_cc_policy(id):
+
+    url = "https://api.dome9.com/v2/Compliance/ContinuousCompliancePolicy/" + id
+    urldata = {}
+    headers = {'content-type': 'application/json'}
+    print('\nUnassociating Compliance Policy...')
+
+    try:
+        resp = requests.delete(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+        
+        # If the response was successful, no Exception will be raised
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success!')
+    
+def delete_notification_policy(id):
+
+    url = "https://api.dome9.com/v2/Compliance/ContinuousComplianceNotification/" + id
+    urldata = {}
+    headers = {'content-type': 'application/json'}
+    print('\nDeleting Notification Policy...')
+
+    try:
+        resp = requests.delete(url, auth=HTTPBasicAuth(d9id, d9secret), json=urldata, headers=headers)
+        
+        # If the response was successful, no Exception will be raised
+        resp.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success!')    
+        
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        sleep(1)
+        t -= 1
 
 # Main
 def main(argv=None):
@@ -170,12 +233,15 @@ def main(argv=None):
         argv = sys.argv[2:]
 #DUMP ARGV log
 
+    mode = sys.argv[1]
+    print("Mode: " + mode)
+
     parser = argparse.ArgumentParser("%prog <aws|azure|gcp> [options] ")
     parser.add_argument("--name", dest="accountname", help="Cloud account friendly name")
     parser.add_argument("--arn", dest="arn", help="AWS Role ARN for Dome9")
     parser.add_argument("--externalid", dest="externalid", help="AWS external ID used for Dome9 Role")
     parser.add_argument("--email", dest="email", help="E-mail Address")
-    parser.add_argument("--sync", dest="sync", help="Time to wait after account onboarded (in minutes)")
+    parser.add_argument("--delay", dest="delay", default="30", help="Time to wait after account onboarded (in minutes). Default: 30.")
 
     OPTIONS = parser.parse_args(argv)
 
@@ -183,8 +249,7 @@ def main(argv=None):
         parser.print_help()
         os._exit(1)
 
-    mode = sys.argv[1]
-    print("Mode: " + mode)
+
 
     if mode == '-h' or mode == '--help':
         parser.print_help()
@@ -195,23 +260,30 @@ def main(argv=None):
         cloudid = 1
         arnparse = OPTIONS.arn.split(':')
         extaccountid = arnparse[4]
-        print('Cloud Account ID: ' + extaccountid)
+        syncwait = int(OPTIONS.delay)
+        print('Cloud Account \n-ID: ' + extaccountid + '\n-Name: ' + OPTIONS.accountname)
         account_added = add_aws_account(OPTIONS.accountname, OPTIONS.arn, OPTIONS.externalid)
 
         if account_added:
-            print('Waiting for initial account sync')
-            #syncwait = OPTIONS.sync * 60
-            #sleep(syncwait)
-            sleep(60)
+            print('\nWaiting ' + OPTIONS.delay + ' minutes for initial account sync to complete.')
+            syncwait = syncwait * 60
+            countdown(syncwait)
+
             notification_name = OPTIONS.accountname + '_' + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(5))
-            notification_policy_added = add_notification_policy(notification_name, OPTIONS.email)
+            utc_datetime = (datetime.utcnow() + timedelta(minutes=5))
+            cronexpression = '0 ' + str(utc_datetime.minute) + ' ' + str(utc_datetime.hour) + ' 1/1 * ? *'
+            notification_policy_added = add_notification_policy(notification_name, OPTIONS.email, cronexpression)
+
             cc_policy_added = add_cc_policy(account_added, extaccountid, notification_policy_added, rulesetid)
             run_assessment(account_added, rulesetid)
-            sleep(5)
-            send_all_alerts(notification_policy_added, cc_policy_added)
+
+            print('\nWaiting 20 minutes to initiate cleanup actions (compliance report should arrive by email by this time).')
+            countdown(1200)
+            unassociate_cc_policy(cc_policy_added)
+            delete_notification_policy(notification_policy_added)
+            remove_cloud_account(account_added)
 
     return 0
-
 
 if __name__ == "__main__":
     sys.exit(main())
